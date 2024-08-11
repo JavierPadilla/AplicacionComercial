65d22178e8d2a8cd6bad260476ff308183eeaa15
JavierPadilla <host_javier@hotmail.com>
2024-08-11 02:02:56 -0600

    Login clase Usuario

--- FrmLogin.Designer.cs
+++ FrmLogin.Designer.cs
@@ -28,18 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            txtUsuario = new TextBox();
+            txtContrasena = new TextBox();
+            btnAceptar = new Button();
+            btnCancelar = new Button();
+            pictureBox1 = new PictureBox();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 106);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(153, 35);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(175, 35);
+            txtUsuario.TabIndex = 2;
+            // 
+            // txtContrasena
+            // 
+            txtContrasena.Location = new Point(153, 106);
+            txtContrasena.Name = "txtContrasena";
+            txtContrasena.Size = new Size(175, 35);
+            txtContrasena.TabIndex = 3;
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Image = Properties.Resources.Aceptar2;
+            btnAceptar.Location = new Point(12, 185);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(131, 90);
+            btnAceptar.TabIndex = 4;
+            btnAceptar.Text = "&Aceptar";
+            btnAceptar.TextAlign = ContentAlignment.BottomCenter;
+            btnAceptar.UseVisualStyleBackColor = true;
+            btnAceptar.Click += btnAceptar_Click;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Image = Properties.Resources.Cancelar;
+            btnCancelar.Location = new Point(186, 185);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(131, 90);
+            btnCancelar.TabIndex = 5;
+            btnCancelar.Text = "&Cancelar";
+            btnCancelar.TextAlign = ContentAlignment.BottomCenter;
+            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Click += btnCancelar_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Ingreso;
+            pictureBox1.Location = new Point(334, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(229, 263);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
             // 
             // FrmLogin
             // 
+            AcceptButton = btnAceptar;
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            CancelButton = btnCancelar;
+            ClientSize = new Size(572, 294);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnAceptar);
+            Controls.Add(txtContrasena);
+            Controls.Add(txtUsuario);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "FrmLogin";
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Ingreso al sistema";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsuario;
+        private TextBox txtContrasena;
+        private Button btnAceptar;
+        private Button btnCancelar;
+        private PictureBox pictureBox1;
+        private ErrorProvider errorProvider1;
     }
 }
--- FrmLogin.cs
+++ FrmLogin.cs
@@ -1,3 +1,5 @@
+using CADAplicacionComercial;
+
 namespace AplicacionComercial
 {
     public partial class FrmLogin : Form
@@ -5,6 +7,39 @@ namespace AplicacionComercial
         public FrmLogin()
         {
             InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text =="")
+            {
+                errorProvider1.SetError(txtUsuario, "Debe ingresar un USUARIO");
+                txtUsuario.Focus();
+                return;
+            }
+            errorProvider1.SetError(txtUsuario, "");
+            if (txtContrasena.Text == "")
+            {
+                errorProvider1.SetError(txtContrasena, "Debe ingresar una contrasena");
+                txtContrasena.Focus();
+                return;
+            }
+            errorProvider1.SetError(txtContrasena, "");
+            if (CADUsuario.ValidarUsuario(txtUsuario.Text, txtContrasena.Text) == true)
+            {
+                MessageBox.Show("Exito", "Entrada al sistema con exito", MessageBoxButtons.OK);
+
+            }
+            else
+            {
+                MessageBox.Show("Usuario o Clave incorrectos", "Error", MessageBoxButtons.OK);
+
+            }
         }
     }
 }
